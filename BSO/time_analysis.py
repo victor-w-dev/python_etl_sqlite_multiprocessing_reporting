8481ef7b14c5a635695129c2b1d719db80c73c84
v-w-dep <54051292+v-w-dep@users.noreply.github.com>
2020-06-26 11:10:27 +0800

    20200626

--- BSO/time_analysis.py
+++ BSO/time_analysis.py
@@ -7,7 +7,7 @@ def time_decorator(method):
         result = method(*args, **kw)
         te = time.time()
 
-        if (te - ts)>=1: print(f'{inspect.getmodule(method)} {method.__name__}\n{(te - ts):2.4f} seconds')
+        if (te-ts)>=1: print(f'{inspect.getmodule(method)} {method.__name__}\n{(te - ts):2.4f} seconds')
         elif (te-ts)>=0.001: print(f'{inspect.getmodule(method)} {method.__name__}\n{(te - ts)*10**3 :2.4f} ms')
         else: print(f'{inspect.getmodule(method)} {method.__name__}\n{(te - ts)/10**6 :2.4f} µs')
         print("-"*100)
--- GeneralTrade.py
+++ GeneralTrade.py
@@ -9,73 +9,72 @@ import os
 import sqlite3 as pyo
 import datetime
 
-
-db_path = "merchandise_trades_DB"
-gold_hs8_code = ("71081100", "71081210", "71081290", "71081300",
-                  "71082010", "71082090", "71090000", "71123000",
-                  "71129100", "71189000")
-
-periods='201812, 201712, 201612, 201512'
-general_trades_sql = f"""
-SELECT A.ReportPeriod,
-       CountryConsignmentCode countrycode,
-       country_name,
-       TX,
-       DX,
-       RX,
-       IM,
-       B.RXbyO,
-       TT,
-       TX - IM TB
-  FROM (
-           SELECT ReportPeriod,
-                  CountryConsignmentCode,
-                  country.[DESC] AS country_name,
-                  sum(DomesticExportValueYTD) AS DX,
-                  sum(ReExportValueYTD) AS RX,
-                  sum(DomesticExportValueYTD + ReExportValueYTD) AS TX,
-                  sum(ImportValueYTD) AS IM,
-                  sum(DomesticExportValueYTD + ReExportValueYTD + ImportValueYTD) AS TT,
-                  sum(ReExportValueYTD) AS RXbyO
-             FROM hsccit
-                  LEFT JOIN
-                  country ON CountryConsignmentCode = country.CODE
-            WHERE TransactionType = 1 AND
-                  ReportPeriod IN ({periods}) AND
-                  hscode NOT IN ("71081100", "71081210", "71081290", "71081300", "71082010", "71082090", "71090000", "71123000", "71129100", "71189000")
-            GROUP BY CountryConsignmentCode,
-                     ReportPeriod
-       )
-       A
-       LEFT JOIN
-       (
-           SELECT ReportPeriod,
-                  CountryOriginCode,
-                  sum(ReExportValueYTD) AS RXbyO
-             FROM hscoccit
-            WHERE TransactionType = 1 AND
-                  ReportPeriod IN ({periods}) AND
-                  hscode NOT IN ("71081100", "71081210", "71081290", "71081300", "71082010", "71082090", "71090000", "71123000", "71129100", "71189000")
-            GROUP BY CountryOriginCode,
-                     ReportPeriod
-       )
-       B ON A.CountryConsignmentCode = B.CountryOriginCode AND
-            A.ReportPeriod = B.ReportPeriod;
-"""
-
-
-
 class GeneralTradeProfile():
-    def __init__(self, startperiod, endperiod):
-        self.start = startperiod
-        self.end = endperiod
+    def __init__(self, periods):
+        self._periods = periods
+
+    @property
+    def periods(self):
+        return self._periods
+
+    @periods.setter
+    def periods(self, periods):
+        if isinstance(periods, tuple):
+            self_periods = periods
+        else:
+            raise TypeError('periods must be tuple')
 
     def get_figures(self,db_path):
         self.con = pyo.connect(db_path+"/"+"trades.db")
-        #self.cursor = self.con.cursor()
-        #self.cursor.execute(general_trades_sql)
-        #rows = self.cursor.fetchall()
-        print(general_trades_sql)
+
+        general_trades_sql = f"""
+        SELECT A.ReportPeriod,
+               CountryConsignmentCode countrycode,
+               country_name,
+               TX,
+               DX,
+               RX,
+               IM,
+               B.RXbyO,
+               TT,
+               TX - IM TB
+          FROM (
+                   SELECT ReportPeriod,
+                          CountryConsignmentCode,
+                          country.[DESC] AS country_name,
+                          sum(DomesticExportValueYTD) AS DX,
+                          sum(ReExportValueYTD) AS RX,
+                          sum(DomesticExportValueYTD + ReExportValueYTD) AS TX,
+                          sum(ImportValueYTD) AS IM,
+                          sum(DomesticExportValueYTD + ReExportValueYTD + ImportValueYTD) AS TT,
+                          sum(ReExportValueYTD) AS RXbyO
+                     FROM hsccit
+                          LEFT JOIN
+                          country ON CountryConsignmentCode = country.CODE
+                    WHERE TransactionType = 1 AND
+                          ReportPeriod IN {self.periods} AND
+                          hscode NOT IN ("71081100", "71081210", "71081290", "71081300", "71082010", "71082090", "71090000", "71123000", "71129100", "71189000")
+                    GROUP BY CountryConsignmentCode,
+                             ReportPeriod
+               )
+               A
+               LEFT JOIN
+               (
+                   SELECT ReportPeriod,
+                          CountryOriginCode,
+                          sum(ReExportValueYTD) AS RXbyO
+                     FROM hscoccit
+                    WHERE TransactionType = 1 AND
+                          ReportPeriod IN {self.periods} AND
+                          hscode NOT IN ("71081100", "71081210", "71081290", "71081300", "71082010", "71082090", "71090000", "71123000", "71129100", "71189000")
+                    GROUP BY CountryOriginCode,
+                             ReportPeriod
+               )
+               B ON A.CountryConsignmentCode = B.CountryOriginCode AND
+                    A.ReportPeriod = B.ReportPeriod;
+        """
+
+        #print(general_trades_sql)
         self.df = pd.read_sql_query(general_trades_sql, self.con)
         return self.df
 
@@ -86,8 +85,17 @@ class GeneralTradeProfile():
 if __name__ == '__main__':
     #calculate time spent
     start_time = time.time()
+    db_path = "merchandise_trades_DB"
+    periods=(201907, 201807, 201812, 201712, 201612, 201512)
+
+    '''
+    gold_hs8_code = ("71081100", "71081210", "71081290", "71081300",
+                      "71082010", "71082090", "71090000", "71123000",
+                      "71129100", "71189000")
+    '''
+
     print("GeneralTradeProfile:")
-    profile = GeneralTradeProfile(2017,2018)
+    profile = GeneralTradeProfile(periods)
     print(profile.get_figures(db_path=db_path))
 
     profile.get_figures(db_path=db_path).to_excel("checking.xlsx")
